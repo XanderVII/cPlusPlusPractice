66208cbbc1741b0ed32cf1087ab2c9b2a61a7382
XanderVII <zeponfire@gmail.com>
2017-05-19 00:50:01 -0500

    Chapter 2 p81-1
    Finished  #1 Sum of Two Numbers program. I am trying out this commit
    command!

---
+++ Chapter 2/p81-1.cpp
@@ -0,0 +1,22 @@
+/* XanderVII
+   Starting out with C++
+   Programming Challenge page 81
+   1. Sum of Two Numbers
+   
+   Objective:
+   Write a program that stores the integers 62 and 99 in variables, and stores the sum of these two in a variable named total.
+*/
+
+#include <iostream>
+using namespace std;
+
+
+// Main Body
+int main()
+{
+	int one = 62;
+	int two = 99;
+	int sum = one + two;
+	
+	cout << sum << endl;	
+}
